e3ad28aa98b1e3f6cc982d0f318f2898396932a1
Natalia Munina <nataly.munina@gmail.com>
2020-02-27 12:54:16 +0100

    added test/JUnit

---
+++ src/com/samuelfb/MainTest.java
@@ -0,0 +1,14 @@
+package com.samuelfb;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void libraryTest() {
+        
+    }
+
+
+}
