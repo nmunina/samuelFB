a5b866529fc3d0486999a5d6cef3276363787f1e
Natalia Munina <nataly.munina@gmail.com>
2020-03-01 21:35:27 +0100

    added .gitignore

--- src/main/java/com/samuelfb/Main.java
+++ src/main/java/com/samuelfb/Main.java
@@ -1,12 +1,17 @@
 package com.samuelfb;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
 
-    public static String input(String s) {
+    public static String input(String input) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         return null;
     }
 }
