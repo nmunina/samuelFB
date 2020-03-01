239c609998dfe1a1895e1448e899e96a4d890030
Natalia Munina <nataly.munina@gmail.com>
2020-03-01 20:19:02 +0100

    added JUnit added pom.xml with dependencies added first test for TDD

--- src/main/java/com/samuelfb/Main.java
+++ src/main/java/com/samuelfb/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static String input(String s) {
+        return null;
+    }
 }
---
+++ src/test/java/com/samuelfb/MainTest.java
@@ -0,0 +1,16 @@
+package com.samuelfb;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void readFromConsole() {
+        String result = Main.input(".-");
+        String expected = "A";
+        Assert.assertEquals(expected, result);
+    }
+
+
+}
