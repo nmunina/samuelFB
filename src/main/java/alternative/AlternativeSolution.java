5a77f4bdc23f3474573b06ae8c17406f630bc91a
Natalia Munina <nataly.munina@gmail.com>
2020-03-03 19:42:37 +0100

    added by Natalia: cleaned code, reverced dictionary

--- src/main/java/alternative/AlternativeSolution.java
+++ src/main/java/alternative/AlternativeSolution.java
@@ -1,16 +1,13 @@
 package alternative;
 
-import com.sun.org.apache.xpath.internal.operations.Equals;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 public class AlternativeSolution {
-    public static HashMap<Character, String> vocabulary;
+    public static HashMap<Character, String> vocabularyEngMorse;
+    public static HashMap<String, Character> vocabularyMorseEng;
     public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
@@ -22,20 +19,23 @@ public class AlternativeSolution {
             System.out.println("Type in your english phrase");
 
             String input = reader.readLine().toLowerCase();
-            char[] toCharArray = input.toCharArray();
+            char[] toCharArray = input.toCharArray(); //Making input String to char[] array
 
-            // Printing array
+            // for each element in Char Array we print out the translated version
             for (char c : toCharArray) {
-                //translate(c);
                 System.out.print(translateToMorse(c) + " ");
             }
         } else if (answer.equals("2")) {
             System.out.println("Type in your morse phrase with spaces between codes. Use dashes(-) and asterisks(*).");
 
+            //TODO: catch if user tries latin letters
             String input = reader.readLine();
 
             //TODO: how to separate words? It looks ugly now
-            String split[] = input.split(" ", 0);
+            //spliting input i separate strings with " "
+            String[] split = input.split(" ", 0);
+
+            // for each element in split find the translation and print it out
             for (String s : split)
                 System.out.print(translateToEng(s));
         }
@@ -44,54 +44,33 @@ public class AlternativeSolution {
     }
 
     private static String welcome() throws IOException {
-
         System.out.println("Welcome to SamuelFB translation. How could we help you?");
         System.out.println("To translate from Enlish to Morse type 1");
         System.out.println("To translate from Morse to English type 2");
 
-        String selection = reader.readLine();
-
-        return selection;
+        return reader.readLine();
     }
+
     // TODO: read about immutable map Map.of('a', "*-")
                                             //'b', "-***")
     private static void initializeVocabulary() {
-        vocabulary = new HashMap<Character, String>();
-        vocabulary.put(' ', " ");
-        vocabulary.put('a', "*-");
-        vocabulary.put('b', "-***");
+        vocabularyEngMorse = new HashMap<Character, String>();
+        vocabularyEngMorse.put(' ', " ");
+        vocabularyEngMorse.put('a', "*-");
+        vocabularyEngMorse.put('b', "-***");
 
+        //Reversing keys and values of Englich-Morse dictionary, to get Morse-Rnglish dictionary
+        vocabularyMorseEng = new HashMap<String, Character>();
+        for (char i : vocabularyEngMorse.keySet()) {
+            vocabularyMorseEng.put(vocabularyEngMorse.get(i), i);
+        }
     }
 
     public static String translateToMorse(char c) {
-        HashMap<Character, String> vocabularyNew = vocabulary;
-
-        Set<Map.Entry<Character, String>> entrySet = vocabularyNew.entrySet();
-        String value = "";
-
-        for (Map.Entry<Character,String> pair : entrySet) {
-            if (pair.getKey().equals(c))
-            {
-                value = pair.getValue();// find out value
-            }
-        }
-
-        return value;
+        return vocabularyEngMorse.get(c);
     }
 
-    public static char translateToEng(String c) {
-        HashMap<Character, String> vocabularyNew = vocabulary;
-
-        Set<Map.Entry<Character, String>> entrySet = vocabularyNew.entrySet();
-        char value = 0;
-
-        for (Map.Entry<Character,String> pair : entrySet) {
-            if (pair.getValue().equals(c))
-            {
-                value = pair.getKey();// find out value
-            }
-        }
-
-        return value;
+    public static char translateToEng(String s) {
+        return vocabularyMorseEng.get(s);
     }
 }
