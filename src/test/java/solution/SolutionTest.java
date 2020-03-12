87b00e99faadcfd76d3b558f7f6ae02e23f02f64
Natalia Munina <nataly.munina@gmail.com>
2020-03-12 12:44:27 +0100

    minor changes

--- src/test/java/solution/SolutionTest.java
+++ src/test/java/solution/SolutionTest.java
@@ -2,7 +2,6 @@ package solution;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class SolutionTest {
@@ -12,62 +11,24 @@ public class SolutionTest {
         Vocabulary.initializeVocabulary();
     }
 
-//    private String translateToMorse(String s) {
-//        return s;
-//    }
-
-//    private String translateToEng(String s) {
-//        return "s";
-//    }
-
     @Test
     public void shouldTranslateMorseToEnglish() {
-        //SolutionTest  obj = new SolutionTest();
-        Character result = Vocabulary.translateToEng(".-");
+        char result = Vocabulary.translateToEng(".-");
         String expected = "a";
         Assert.assertEquals(expected, Character.toString(result));
     }
 
     @Test
     public void shouldTranslateEnglishToMorse() {
-        //SolutionTest  obj = new SolutionTest();
         String result = Vocabulary.translateToMorse('a');
         String expected = ".-";
         Assert.assertEquals(expected, result);
     }
 
-
-//    @Test
-//    public void shouldReturnTrue() {
-//        //SolutionTest obj = new SolutionTest();
-//        String result = Vocabulary.translateToMorse("abc");
-//        String expected ="*- -*** -*-*";
-//        Assert.assertEquals(expected, result);
-//    }
-
-
-//    @Test
-//    public void shouldReturnTrueWhenTranslateToMorse() {
-//        SolutionTest obj = new SolutionTest();
-//        String result = obj.translateToMorse("abc");
-//        String expected ="*- -*** -*-*";
-//        Assert.assertEquals(expected, result);
-//    }
-
-    //
-//    @Test
-//    public void shouldReturnInvalid() {
-//        //SolutionTest obj = new SolutionTest();
-//        String result = Vocabulary.translateToEng( "*");
-//        String expected ="Invalid";
-//        Assert.assertNotEquals(expected, result);
-//    }
-
-//    @Test
-//    public void shouldReturnValidWhenTranslatingToEng() {
-//        SolutionTest obj = new SolutionTest();
-//        String result = obj.translateToEng( "*");
-//        String expected = "e";
-//        Assert.assertEquals(expected, result);
-//    }
+    @Test
+    public void shouldNotTranslateEnglishToMorse() {
+        String result = Vocabulary.translateToMorse('a');
+        String expected = ".--";
+        Assert.assertNotEquals(expected, result);
+    }
 }
